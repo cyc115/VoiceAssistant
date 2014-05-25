08e40256d879b769e9c13e0b5bfec73075290518
yuechuanchen <yuechuan20@gmail.com>
2014-05-25 08:22:15 -0400

    defined skeleton of HelpCommand

---
+++ src/com/example/command/HelpCommand.java
@@ -0,0 +1,12 @@
+package com.example.command;
+/**
+ * TODO user can say help and this should generate a 
+ * tts help menu that can be navigated.the menu will 
+ * help the user to learn commands and general usecases
+ * @author yuechuan
+ * TODO unimplemented 
+ *
+ */
+public class HelpCommand implements CommandActions {
+
+}
